fbc5609cfe72e88e3ce1ab3bd10a716b7b302927
Felix Criollo <felixcriollovigo@gmail.com>
2022-01-29 18:17:47 -0500

    setting style

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      El que quiere pan comque queso
     </div>
   );
 }
---
+++ src/components/style/styleVariables.js
@@ -0,0 +1,10 @@
+export const styleVariables = {
+  "primary": "hsl(26, 100%, 55%)",
+  "primary-bg": "hsl(25, 100%, 94%)",
+  "text-hea": "hsl(220, 13%, 13%)",
+  "text-para": "hsl(219, 9%, 45%)",
+  "text-price": "hsl(220, 14%, 75%)",
+  "bg": "hsl(0, 0%, 100%)",
+  "bg-input": "hsl(223, 64%, 98%)",
+  "ligthbox-bg": "rgba(0, 0, 0, 0.75)"
+}
